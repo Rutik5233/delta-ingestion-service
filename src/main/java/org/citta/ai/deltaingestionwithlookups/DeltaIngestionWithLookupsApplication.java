abe74cd7d955f963d8fb0773450e7d8e51c8f4ba
Rutik5233 <rutikkatkamwar12334@gmail.com>
2026-04-14 12:50:08 +0530

    feat: initial delta ingestion service

---
+++ src/main/java/org/citta/ai/deltaingestionwithlookups/DeltaIngestionWithLookupsApplication.java
@@ -0,0 +1,13 @@
+package org.citta.ai.deltaingestionwithlookups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaIngestionWithLookupsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeltaIngestionWithLookupsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/citta/ai/deltaingestionwithlookups/DeltaIngestionWithLookupsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.citta.ai.deltaingestionwithlookups;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaIngestionWithLookupsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
