6431e45a26c7ac451845a6bde3f0948d7ddb4d6e
Rutik5233 <rutikkatkamwar12334@gmail.com>
2026-04-15 22:33:00 +0530

    Add postman jsons and resolve swagger issue

--- src/main/java/com/ingestion/config/OpenApiConfig.java
+++ src/main/java/com/ingestion/config/OpenApiConfig.java
@@ -5,12 +5,8 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 @SecurityScheme(
@@ -21,13 +17,9 @@ import java.util.List;
 )
 public class OpenApiConfig {
 
-    @Value("${app.server-url:http://localhost:8080")
-    private String serverUrl;
-
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .servers(List.of(new Server().url(serverUrl)))
                 .info(new Info()
                         .title("Delta Ingestion Service API")
                         .version("1.0")
